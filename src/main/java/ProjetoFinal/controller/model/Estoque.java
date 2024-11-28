912cb66513570fac2854e5fe3d938691e3901a61
ariellucaslf <142851546+ariellucaslf@users.noreply.github.com>
2024-11-28 00:53:49 -0300

    organização das pasta do projeto adição de classe
    pastas do projeto organizadas, e adição da classe estoque

---
+++ src/main/java/ProjetoFinal/controller/model/Estoque.java
@@ -0,0 +1,17 @@
+public class Estoque {
+    private Item item;
+    private int quantidade;
+
+    public Estoque(Item item, int quantidade) {
+        this.item = item;
+        this.quantidade = quantidade;
+    }
+
+    public void atualizarQuantidade(int novaQuantidade) {
+        this.quantidade = novaQuantidade;
+    }
+
+    public String getStatus() {
+        return "Item: " + item.getNome() + ", Quantidade: " + quantidade;
+    }
+}
