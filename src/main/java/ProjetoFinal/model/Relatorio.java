4a6d76e72ccd571d41a2e86e6a265529ba83ebe4
Micael B. Alves <micaelmepb2024@gmail.com>
2024-11-28 19:02:43 -0300

    Criei a classe "Relatorio" na pasta 'model'

---
+++ src/main/java/ProjetoFinal/model/Relatorio.java
@@ -0,0 +1,16 @@
+public class Relatorio {
+
+    public void gerarRelatorioEstoque(ArrayList<Estoque> estoques) {
+        System.out.println("Relatório de Estoque:");
+        for (Estoque estoque : estoques) {
+            System.out.println(estoque.getStatus());
+        }
+    }
+
+    public void gerarRelatorioPedidos(ArrayList<Pedido> pedidos) {
+        System.out.println("Relatório de Pedidos:");
+        for (Pedido pedido : pedidos) {
+            System.out.println(pedido.getDetalhesPedido());
+        }
+    }
+}
