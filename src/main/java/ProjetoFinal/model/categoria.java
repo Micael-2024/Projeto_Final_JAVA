4b012cdd9b1ecfe99e7abfd4c149d26f8fc322a8
Micael B. Alves <micaelmepb2024@gmail.com>
2024-11-28 15:01:39 -0300

    Criei a classe categoria na pasta "model"

---
+++ src/main/java/ProjetoFinal/model/categoria.java
@@ -0,0 +1,13 @@
+public class Categoria {
+    private String nome;
+    private int id;
+
+    public Categoria(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return "Categoria: " + nome + " (ID: " + id + ")";
+    }
+}
