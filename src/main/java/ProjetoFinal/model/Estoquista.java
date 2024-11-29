99d086da6c5a397a36eb950a435e2aca88b2d6b7
Micael B. Alves <micaelmepb2024@gmail.com>
2024-11-29 15:01:00 -0300

    Criei classe "Estoquista"

---
+++ src/main/java/ProjetoFinal/model/Estoquista.java
@@ -0,0 +1,10 @@
+public class Estoquista extends Funcionario {
+    public Estoquista(String nome, int id) {
+        super(nome, "Estoquista", id);
+    }
+
+    @Override
+    public void realizarTarefa() {
+        System.out.println(getNome() + " está organizando itens no estoque.");
+    }
+}
